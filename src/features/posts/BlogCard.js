6764dde678dd4e9c521628186b4544fba56401a7
rahul122703 <killbusynes@gmail.com>
2025-07-08 01:58:44 +0530

    adding new feature

--- src/features/posts/BlogCard.js
+++ src/features/posts/BlogCard.js
@@ -8,30 +8,30 @@ const BlogCard = ({ id, title, content, userid, reaction }) => {
   const dispatch = useDispatch();
 
   return (
-    <div
-      key={id}
-      className="relative bg-white shadow-lg rounded-xl p-6 border border-gray-300 w-full max-w-[25rem] min-h-[320px] flex flex-col justify-between">
-      <button
-        className="p-1 rounded-full absolute bg-red-400 hover:bg-red-700 border border-black top-2 right-2"
-        aria-label="Delete"
-        onClick={() => dispatch(deletePost(id))}>
-        ✖
-      </button>
+    <div className="relative w-full max-w-[25rem] min-h-[320px]">
+      <div className="absolute -top-4 -right-2 flex gap-2 z-10">
+        <button
+          className="p-1 rounded-full bg-yellow-400 hover:bg-yellow-600 border border-black"
+          aria-label="Edit"
+          onClick={() => dispatch(startEditing(id))}>
+          ✏️
+        </button>
+        <button
+          className="p-1 rounded-full bg-red-400 hover:bg-red-700 border border-black"
+          aria-label="Delete"
+          onClick={() => dispatch(deletePost(id))}>
+          ✖
+        </button>
+      </div>
 
-      <button
-        className="p-1 rounded-full absolute bg-yellow-400 hover:bg-yellow-600 border border-black top-2 right-12"
-        aria-label="Edit"
-        onClick={() => dispatch(startEditing(id))}>
-        ✏️
-      </button>
-
-      <h2 className="text-xl font-bold text-gray-800 line-clamp-1">{title}</h2>
-
-      <p className="text-gray-600 mt-2 line-clamp-3">{content}</p>
-
-      <ReactionButtons blogid={id} reaction={reaction} />
-
-      <UsersComponent userid={userid} />
+      <div className="bg-white shadow-lg rounded-xl p-6 border border-gray-300 w-full h-full flex flex-col justify-between">
+        <h2 className="text-xl font-bold text-gray-800 line-clamp-1">
+          {title}
+        </h2>
+        <p className="text-gray-600 mt-2 line-clamp-3">{content}</p>
+        <ReactionButtons blogid={id} reaction={reaction} />
+        <UsersComponent userid={userid} />
+      </div>
     </div>
   );
 };
